e363a5472d5406bddf7cdd0e9a669d7abf58d2a0
Nanjundappa <rnanjund@informatica.com>
2016-07-22 15:37:52 +0530

    commit control structures

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,19 @@
 package control_structures
 
+import "fmt"
+
 func fizzBuzz(i int32) string {
-	return ""
+	if i%3 == 0 && i%5 == 0 {
+		return "FizzBuzz"
+	}
+
+	if i%3 == 0 {
+		return "Fizz"
+	}
+
+	if i%5 == 0 {
+		return "Buzz"
+	}
+
+	return fmt.Sprintf("%d", i)
 }
