fed5a3c1961df2a11a230cf0fd3e8d976fa7a252
raghu83sn <raghubond@gmail.com>
2016-07-22 16:22:55 +0530

    t changes

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,23 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	var count int
+	count = 1
+	recCollatz(n, &count)
+	return count
+}
+
+func recCollatz(n int, count *int) int {
+	if n == 1 {
+		return 1
+	} else if n%2 == 0 {
+		(*count)++
+		return recCollatz(n/2, count)
+	} else if n%2 == 1 {
+		(*count)++
+		return recCollatz(3*n+1, count)
+	} else {
+		return n
+	}
+
 }
