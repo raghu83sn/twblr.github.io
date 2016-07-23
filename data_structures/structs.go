645c6249e0ef8df301777e1186874a3d977940b3
raghu83sn <raghubond@gmail.com>
2016-07-23 10:14:49 +0530

    one more checkin

--- data_structures/structs.go
+++ data_structures/structs.go
@@ -2,12 +2,32 @@ package data_structures
 
 // Linked List Implementation
 
-type Node struct{}
+type NodeHeader struct {
+	start *Node,
+	last *Node
+}
+
+type Node struct{
+	value int,
+	leftNode *Node,
+	rightNode *Node
+	nodeHeader *NodeHeader
+}
+
+
 
 func Add(elem int) {
 	return
 }
 
 func Search(elem int) *Node {
+	next_node *int
+	for i := 1; i <= elem; i++ {
+		if(value != nil) {
+			return 
+		}
+	}
+
+
 	return nil
 }
